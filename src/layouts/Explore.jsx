724dcadba63e13b9ba890f4f8a378ba5f4fae4c9
Mahdi Alikhani <01Skull01@gmail.com>
2024-05-27 21:21:41 +0330

    -a

---
+++ src/layouts/Explore.jsx
@@ -0,0 +1,104 @@
+import React, { useState } from "react";
+
+import { IoMdHeart } from "react-icons/io";
+import { GoPlus } from "react-icons/go";
+
+import "./Explore.scss";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/scss";
+
+import Card from "../components/Card";
+
+const Explore = () => {
+  const [productList, setProductList] = useState([
+    {
+      id: 1,
+      src: "/assets/images/t-shirt.jpg",
+      alt: "t-shirt",
+      title: "Black T-Shirt",
+      description: "Description",
+      price: 250,
+      add: <GoPlus />,
+      favourite: true,
+    },
+
+    {
+      id: 2,
+      src: "/assets/images/jeans.jpg",
+      alt: "jeans",
+      title: "Blue jeans",
+      description: "Description",
+      price: 115,
+      add: <GoPlus />,
+      favourite: false,
+    },
+
+    {
+      id: 3,
+      src: "/assets/images/shoes.jpg",
+      alt: "shoes",
+      title: "Orange Shoes",
+      description: "Description",
+      price: 125,
+      add: <GoPlus />,
+      favourite: false,
+    },
+  ]);
+
+  return (
+    <div className="explore-container">
+      <h2> Explore </h2>
+      <div className="slider-section">
+        <Swiper
+          className="cards-slider"
+          loop={true}
+          spaceBetween={0}
+          slidesPerView={2}
+          onSwiper={(swiper) => console.log(swiper)}
+        >
+          {productList.map((product) => (
+            <SwiperSlide className="cards-slide" key={product.id}>
+              <Card
+                src={product.src}
+                alt={product.alt}
+                title={product.title}
+                description={product.description}
+                price={product.price}
+                add={product.add}
+                favourite={
+                  product.favourite && (
+                    <div className="favourite">
+                      <IoMdHeart />
+                    </div>
+                  )
+                }
+              />
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
+    </div>
+  );
+};
+
+export default Explore;
+
+{
+  /* <SwiperSlide className="cards-slide">
+<Card
+  src={"/assets/images/t-shirt.jpg"}
+  alt={"t-shirt"}
+  title={"Black T-Shirt"}
+  description={"Description"}
+  price={250.0}
+  add={<GoPlus />}
+/>
+</SwiperSlide>
+<SwiperSlide className="cards-slide">
+<Card />
+</SwiperSlide>
+<SwiperSlide className="cards-slide">
+<Card />
+</SwiperSlide> */
+}
---
+++ src/main.jsx
@@ -0,0 +1,76 @@
+import React, { useState } from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import App from "./pages/Home.jsx";
+import Products from "./pages/Products.jsx";
+import PageNotFound from "./pages/404.jsx";
+
+import ProductDetails from "./components/ProductDetails.jsx";
+
+const routes = [
+  {
+    url: "/products/tshirt",
+    id: 1,
+    src: "/assets/images/t-shirt.jpg",
+    alt: "t-shirt",
+    price: 250,
+    title: "Black T-Shirt",
+    firstColor: "black",
+    secondColor: "gray",
+    thridColor: "blue",
+  },
+
+  {
+    url: "/products/jeans",
+    id: 2,
+    src: "/assets/images/jeans.jpg",
+    alt: "Blue Jeans",
+    price: 115,
+    title: "Black T-Shirt",
+    firstColor: "blue",
+    secondColor: "black",
+    thridColor: "orange",
+  },
+  {
+    url: "/products/shoes",
+    id: 3,
+    src: "/assets/images/shoes.jpg",
+    alt: "shoes",
+    price: 125,
+    title: "Orange Shoes",
+    firstColor: "orange",
+    secondColor: "purple",
+    thridColor: "pink",
+  },
+];
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/products" element={<Products />} />
+        <Route path="/" element={<App />} />
+        {routes.map((detail) => (
+          <Route
+            key={detail.id}
+            path={detail.url}
+            element={
+              <ProductDetails
+                src={detail.src}
+                alt={detail.alt}
+                price={detail.price}
+                title={detail.title}
+                firstColor={detail.firstColor}
+                secondColor={detail.secondColor}
+                thirdColor={detail.thridColor}
+                url={'/products/tshirt'}
+              />
+            }
+          />
+        ))}
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
